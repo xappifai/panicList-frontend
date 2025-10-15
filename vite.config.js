f5b02f81d264b62013855c82695e6e89682d5cfa
CodeX93 <xappifai@gmail.com>
2025-10-15 11:28:06 +0500

    v1.0.1-fixed start script

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  },
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
 })
